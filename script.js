939a2ad5156dd7d2cecee21003fc257a3ad7ef10
nadia <nadiatap@gmail.com>
2016-10-12 20:51:09 -0300

    trabajo js leccion20

---
+++ script.js
@@ -0,0 +1,13 @@
+// funcion promedio
+function callbackPromedio (a, b, c, callback){
+
+	var nota =( a + b + c)/ 3;
+	return callback (nota);
+}
+// funcion secundero
+var segundo = 1;
+function segundero (){
+	document.getElementById("titulo").innerHTML = segundo++;
+
+}
+setInterval(segundero,1000);
